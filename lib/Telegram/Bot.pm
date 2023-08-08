c6c6c94de9f8093c577bbae4503af4677e4103e4
Rev. Duncan Ross Palmer <2e0eol@gmail.com>
2023-08-08 22:54:02 +0100

    Bump version to 1.3.0

--- lib/Telegram/Bot.pm
+++ lib/Telegram/Bot.pm
@@ -23,7 +23,7 @@ use POSIX;
 use utf8;
 
 BEGIN {
-	our $VERSION = '1.2.0';
+	our $VERSION = '1.3.0';
 }
 
 Readonly my $WEATHER_API_TOKEN => 'Cj1MKv18bAcUYSIyjZnrpckLv'; # FIXME: Redacted; you need to patch this with guilt until we have a config mechanism
