c91b15fa119b2e8897c333c574a0a24019bb41a1
Rev. Duncan Ross Palmer <2e0eol@gmail.com>
2023-08-19 11:32:22 +0100

    Initial Dependency Injection Container skeleton
    The idea here is to list everything possible and then remove
    don't need.
    
    The ultimate idea is to make available, globally, all of the major
    parts which need to inter-operate, and so they may be tested using
    dependency injection.

---
+++ lib/Telegram/Bot/DI/Container.pm
@@ -0,0 +1,63 @@
+# telegram-bot
+# Copyright (c) 2023, Rev. Duncan Ross Palmer (2E0EOL),
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  1. Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#
+#  2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#
+#  3. Neither the name of the project nor the names of its contributors
+#     may be used to endorse or promote products derived from this software
+#     without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE PROJECT AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE PROJECT OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+package Telegram::Bot::DI::Container;
+use Moose;
+
+use Readonly;
+
+#has __db => (is => 'ro', isa => 'Telegram::Bot::DB', init_arg => 'db', required => 1);
+
+#lib/Telegram/Bot/Weather/Location.pm
+#lib/Telegram/Bot/Memes/Add/Resizer.pm
+#lib/Telegram/Bot/Memes/Add.pm
+#lib/Telegram/Bot/Memes/Handle.pm
+#lib/Telegram/Bot/Admin.pm
+#lib/Telegram/Bot/Admins.pm
+#lib/Telegram/Bot/Audit.pm
+#lib/Telegram/Bot/Ball8.pm
+#lib/Telegram/Bot/Base.pm
+#lib/Telegram/Bot/CatClient.pm
+#lib/Telegram/Bot/Config/Section.pm
+#lib/Telegram/Bot/DB.pm
+#lib/Telegram/Bot/DrinksClient.pm
+#lib/Telegram/Bot/GenderClient.pm
+#lib/Telegram/Bot/Memes.pm
+#lib/Telegram/Bot/MusicDB.pm
+#lib/Telegram/Bot/RandomNumber.pm
+#lib/Telegram/Bot/UUIDClient.pm
+#lib/Telegram/Bot/User.pm
+#lib/Telegram/Bot/User/Repository.pm
+#lib/Telegram/Bot/Config.pm
+#lib/Telegram/Bot/DI/Container.pm
+#lib/Telegram/Bot.pm
+
+1;
