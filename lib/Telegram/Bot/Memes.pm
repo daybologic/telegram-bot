15cb487b02dd1d3a9906c105506c650e1b8005c9
Rev. Duncan Ross Palmer <2e0eol@gmail.com>
2023-08-23 09:52:19 +0100

    SECURITY: Better detaint for /../../ in Memes
    There is a security flaw here where files in parents of the cache
    can be accessed, ultimately to any accessible file in the filesystem
    under the UID of the bot; however, it is limited in scope because it
    must be an image, and you also need to know the name of the image,
    effectively blindly.

--- lib/Telegram/Bot/Memes.pm
+++ lib/Telegram/Bot/Memes.pm
@@ -257,8 +257,11 @@ sub __pathFromCache {
 sub __detaint {
 	my ($command) = @_;
 	$command = substr($command, 1) if (index($command, '/') == 0);
-	$command = $1 if ($command =~ m/^([a-z0-9]+)/i);
-	return lc($command);
+	if ($command =~ m/^([a-z0-9]+)/i) {
+		return lc($1);
+	}
+
+	return '_unknown';
 }
 
 sub __generateS3URI {
