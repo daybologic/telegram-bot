5c16d07616718f04adee89f3b775a3c1b912bf35
Rev. Duncan Ross Palmer <2e0eol@gmail.com>
2023-08-26 10:36:08 +0100

    Check if you're running the bot as root and abort

--- bin/m6kvmdlcmdr.pl
+++ bin/m6kvmdlcmdr.pl
@@ -1,12 +1,27 @@
 #!/usr/bin/env perl
 
 package main;
-use Telegram::Bot;
 use strict;
 use warnings;
+use English qw(-no_match_vars);
+use POSIX qw(EXIT_SUCCESS);
+use Readonly;
+#use Telegram::Bot;
+
+Readonly my $INSTALLED_ERROR => 42; # arbitary
 
 sub main {
+	if (my $exitCode = rootCheck()) {
+		print(STDERR "Don't run the bot as the super-user!\n");
+		return $exitCode;
+	}
+
 	return Telegram::Bot->new->run();
+}
+
+sub rootCheck {
+	return $INSTALLED_ERROR if ($EUID == 0);
+	return EXIT_SUCCESS;
 }
 
 exit(main());
