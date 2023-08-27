8fa99da77f18de6e02a2164cdb7d549782ec08eb
Rev. Duncan Ross Palmer <2e0eol@gmail.com>
2023-08-27 11:04:34 +0100

    Move /tmp/palmer to /var/cache/telegram-bot

--- lib/Telegram/Bot/CatClient.pm
+++ lib/Telegram/Bot/CatClient.pm
@@ -41,6 +41,8 @@ use URI::Encode;
 
 Readonly my $CAT_URL => 'https://http.cat/%d';
 
+has rootPath => (is => 'ro', isa => 'Str', default => '/var/cache/telegram-bot/cat_client');
+
 sub run {
 	my ($self, $code) = @_;
 	return undef unless ($code);
@@ -65,9 +67,8 @@ sub run {
 
 sub __getFile {
 	my ($self, $code, $content) = @_;
-	mkdir('/tmp/palmer');
-	mkdir('/tmp/palmer/m6kvmdlcmdr');
-	my $name = sprintf('/tmp/palmer/m6kvmdlcmdr/%d.jpg', $code);
+	mkdir($self->rootPath);
+	my $name = sprintf('%s/%d.jpg', $self->rootPath, $code);
 	return $name if -f $name;
 
 	return undef unless ($content);
